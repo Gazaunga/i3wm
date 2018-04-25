43019089316b3c47841fb1db32941a39adcdab27
Gazaunga <Gazaunga@gmail.com>
2018-04-25 17:15:00 -0400

    Update deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -5,7 +5,23 @@ require 'fileutils'
 DESTINATION = "#{Dir.home}/"
 
 apps = %w[
-
+  mpd
+  ncmpcpp
+  zsh
+  dunst
+  i3-gaps-git
+  polybar-git
+  qutebrowser
+  firefox
+  rxvt-unicode
+  emacs
+  feh
+  youtube-dl-git
+  i3lock-color
+  compton-git
+  zathura
+  zathura-pdf-mupdf
+  newsboat-git
 ]
 
 install = ->app{ `trizen -Syu --noconfirm #{app}` }
@@ -18,6 +34,7 @@ end
 def grab_fonts
 status = system("git clone https://github.com/Gazaunga/.fonts ~/.fonts")
 puts status ? "Success" : "Failed"
+`fc-cache -fv`
 end
 
 def make_spacemacs
@@ -31,4 +48,4 @@ grab_fonts
 make_spacemacs
 Dir.glob("#{Dir.home}/i3wm/.", File::FNM_DOTMATCH).each { |f| FileUtils.cp_r("#{f}", DESTINATION, :verbose => true) }
 
-#apps.each &install
+apps.each &install
